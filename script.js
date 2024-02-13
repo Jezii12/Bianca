522fb8c60bdd6830c7bd6a0c639b77c90804f5df
Jerus Zion Sanqui <87622389+Jezii12@users.noreply.github.com>
2024-02-14 01:01:46 +0800

    try

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
-$(document).ready(function () {
-    $('.container').mouseenter(function () {
-        $('.card').stop().animate({
-            top: '-90px'
-        }, 'slow');
-    }).mouseleave(function () {
-        $('.card').stop().animate({
-            top: 0
-        }, 'slow');
-    });
-});
+var clickMeAgainClicked = false;
+
+function showClickMeAgain() {
+    if (!clickMeAgainClicked) {
+        clickMeAgainClicked = true;
+
+        // Create "Click Me Again" button
+        var clickMeAgainBtn = document.createElement("button");
+        clickMeAgainBtn.innerText = "Click Me Again";
+        clickMeAgainBtn.id = "clickMeAgainBtn";
+        clickMeAgainBtn.onclick = showFinalButton;
+
+        // Append the button to the body
+        document.body.appendChild(clickMeAgainBtn);
+    }
+}
+
+function showFinalButton() {
+    // Create "Now, Click Me" button
+    var finalButton = document.createElement("button");
+    finalButton.innerText = "Now, Click Me";
+    finalButton.id = "finalButton";
+    finalButton.onclick = showMessage;
+
+    // Append the button to the body
+    document.body.appendChild(finalButton);
+}
+
+function showMessage() {
+    // Display a message
+    alert("Congratulations! You clicked all the buttons!");
+}
