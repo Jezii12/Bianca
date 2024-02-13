e4f6719710a621a1098b5e8b508d4f87f904d0fd
Jerus Zion Sanqui <87622389+Jezii12@users.noreply.github.com>
2024-02-13 23:52:58 +0800

    try

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
 $(document).ready(function () {
-    $('.container').mouseenter(function () {
-      $('.card').stop().animate({
-        top: '-90px'
-      }, 'slow');
-    }).mouseleave(function () {
-      $('.card').stop().animate({
-        top: 0
-      }, 'slow');
+    $('.envelope').click(function () {
+      $('.letter').toggleClass('open');
     });
   });
-  
