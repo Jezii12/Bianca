4d609a1040a0e23d0d3a9d7201ebc884fe587703
Jerus Zion Sanqui <zionsanqui@gmail.com>
2024-05-16 06:21:13 +0800

    try

--- script.js
+++ script.js
@@ -14,6 +14,11 @@ function openModal() {
     var modal = document.getElementById("myModal");
     modal.style.display = "block";
   }
+
+  function openModal2() {
+    var modal = document.getElementById("myModal2");
+    modal.style.display = "block";
+  }
   
   function closeModal() {
     var modal = document.getElementById("myModal");
